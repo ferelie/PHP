1f3b8a0ca07bbc1f53cbc87cee3485bf4a588ae2
Abdallah_Yasser <43456424+ferelie@users.noreply.github.com>
2024-06-23 15:02:45 +0300

    Update index.php

--- day1/index.php
+++ day1/index.php
@@ -21,35 +21,11 @@ if ($age < 5) {
 else if ($age == 5) {
     echo "Go to kindergarten";
 } 
-
-else if ($age >= 6 && $age <= 12) {
     
-    switch ($age) {
-        case 6:
-            echo "Go to Grade 1";
-            break;
-        case 7:
-            echo "Go to Grade 2";
-            break;
-        case 8:
-            echo "Go to Grade 3";
-            break;
-        case 9:
-            echo "Go to Grade 4";
-            break;
-        case 10:
-            echo "Go to Grade 5";
-            break;
-        case 11:
-            echo "Go to Grade 6";
-            break;
-        case 12:
-            echo "Go to Grade 7";
-            break;
-        default:
-            echo "Invalid age";
-    }
-} 
+else if ($age >= 6 && $age <= 60) {
+    
+    $grade = $age - 5;
+    
 else {
     echo "Too old for school";
 }
